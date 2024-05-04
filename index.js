d6edf046d97f908bcb1a8307b5a1c10259c967b7
pdavidborges <112510561+pdavidborges@users.noreply.github.com>
2024-05-03 21:48:57 -0300

    tipos de dados

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
 
-app.listen(1234)
+// string
+// number
+// object
+// array
+
+app.get('/users', (req, res) => {
+    const isString = 'este é um teste'
+    console.log('isString é to tipo ' + typeof isString)
+
+    const isNumber = 1234
+    console.log('isNumber é to tipo ' + typeof isNumber)
+
+    const isObject = {
+        nome: 'Vitor',
+        cpf: '11644821737',
+        dataNascimento: '07/04/1986',
+        dataAtual: new Date(),
+        endereco: {
+            rua: 'ferreira de menezes',
+            numero: 155
+        },
+        pedidos: [1, 5, 56, 98],
+        chamar: () => {
+            return 'Cola ae, Vitor!'
+        }
+    }
+    console.log(isObject.chamar())
+    console.log('isObject é to tipo ' + typeof isObject)
+    console.log(isObject)
+                     // 0     // 1     // 2
+    const isArray = ['Cássio', 'Leo', 'Rafa']
+    console.log('isArray é to tipo ' + typeof isArray)
+
+
+
+    return res.json({
+        mensagem: 'Coletar informações do meu servidor'
+    });
+})
+
+app.post('/users', (req, res) => {
+    return res.json({
+        mensagem: 'Entregar informações para serem tratadas no meu servidor',
+        informacao: 'O ' + req.body.nome + ' prefere home office'
+    });
+});
+
+app.listen(1234, () => {
+    console.log('Servidor ON')
+})
