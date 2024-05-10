41baa50d60582a6ebe8a48f8c933420c0a5f6a29
pdavidborges <112510561+pdavidborges@users.noreply.github.com>
2024-05-10 19:52:05 -0300

    CRUD node em memória

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
-const alunos = [
+let alunos = [
     {
+        id: 1,
         nome: "Rafa"
     },
     {
+        id: 2,
         nome: "Leo"
     },
     {
+        id: 3,
         nome: "Cássio"
     }
 ]
@@ -25,6 +28,34 @@ app.post('/users', (req, res) => {
     })
     return res.json(alunos);
 });
+
+app.put('/users/:id', (req, res) => {
+    console.log(req.params.id)
+
+    // meu contador vai começar na posição ZERO (let i = 0;)
+    // enquando o contador for menor do que o tamanho do meu array (i < alunos.length)
+    // eu incremento o meu contador em 1 posição para frente (i = i + 1)
+    for(let i = 0; i < alunos.length; i = i + 1) {
+        // console.log(alunos[i].nome + ' - ' + alunos[i].id);
+        if(alunos[i].id == req.params.id) {
+            alunos[i].nome = req.body.nome
+        }
+    }
+    
+    return res.json({
+        msg: 'atualizado com sucesso!'
+    });
+})
+
+app.delete('/users/:id', (req, res) => {
+    alunos = alunos.filter((item) => {
+        return item.id != req.params.id
+    })
+
+    return res.json({
+        msg: 'removido com sucesso!'
+    });
+})
 
 app.listen(1234, () => {
     console.log('Servidor ON')
