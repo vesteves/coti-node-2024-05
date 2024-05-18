276d39cc83477e1337f20a18c224c8aed87b1da7
Vitor Esteves <vitordasilvaesteves@gmail.com>
2024-05-17 22:20:24 -0300

    Aula 4

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,6 +1,25 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
 
 const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+mongoose.connect('mongodb://127.0.0.1:27017/local');
+
+const Aula1Schema = new mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    nome: String,
+},
+{
+    timestamps: true,
+    versionKey: false
+})
+
+const Aula1Model = mongoose.model('aula1', Aula1Schema)
 
 type Mare = 'alta' | 'baixa'
 
@@ -43,6 +62,37 @@ app.get('/health', (req, res) => {
     return res.json({
         msg: 'ok'
     })
+})
+
+app.get('/', async (req, res) => {
+    const result = await Aula1Model.find({});
+    return res.json(result)
+})
+
+app.post('/', async (req, res) => {
+    console.log(req.body)
+    const nome = req.body.nome
+    const result = await Aula1Model.create({
+        _id: new mongoose.Types.ObjectId,
+        nome
+    })
+    return res.json(result)
+})
+
+app.delete('/:_id', async (req, res) => {
+    const result = await Aula1Model.deleteOne({
+        _id: req.params._id
+    })
+    return res.json(result)
+})
+
+app.put('/:_id', async (req, res) => {
+    const result = await Aula1Model.updateOne({
+        _id: req.params._id
+    }, {
+        nome: req.body.nome
+    })
+    return res.json(result)
 })
 
 app.listen(8000, () => {
